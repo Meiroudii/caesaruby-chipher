2ca3029cffbb393ba473e276e3ac32c5d9a89355
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-08-04 01:49:00 +0900

    Add: user inputs for encrypting texts

--- main.rb
+++ main.rb
@@ -8,3 +8,11 @@ def decrypt(string, key)
 end
 
 puts encrypt("hello", 1)
+
+puts "CEASAR CIPHER"
+print "Enter want to encrypt: "
+string = gets.chomp.to_s
+print "Enter key(numbers only): "
+key = gets.chomp.to_i
+
+puts encrypt(string, key)
